6cf1d2a663b4d4c0ae1a38da9bbeb373c4793e0b
DiosAkhilleus <abertin1809@gmail.com>
2021-03-15 09:01:51 -0500

    added webpack, removed unnecessary functionality

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import {  toFahrenheit } from './math'
+
+async function getWeather (city, state, country) {
+    const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&units=imperial&APPID=0728c611df00b978be5fae0e86bdf9a8`, {mode:'cors'});
+    const weatherData = await weather.json();
+    console.log(weatherData);
+    dataSet(weatherData);
+};
+
+const dataSet = (info) => {
+    
+    let currentTemp = `Current Temp: ${Math.round(info.main.temp)}\xB0F`;
+    let maxTemp = `Today's Max: ${Math.round(info.main.temp_max)}\xB0F`;
+    let minTemp = `Today's Min: ${Math.round(info.main.temp_min)}\xB0F`;
+    let feelsLike = `Feels Like: ${Math.round(info.main.feels_like)}\xB0F`;
+    let humidity = `Humidity: ${info.main.humidity}%`;
+    console.log(currentTemp, maxTemp, minTemp, feelsLike, humidity);
+}
+
+getWeather('Austin', 'TX');
--- src/math.js
+++ src/math.js
@@ -2,11 +2,15 @@ const KELVIN = 273.15;
 
 const toFahrenheit = (temp) => {
     let cels = temp - KELVIN;
-    let rounded = round(cels, 1);
-    return rounded;
+    let fahr = (cels * 9/5) + 32;
+    let rounded = Math.round(fahr);
+    return rounded; 
 }
 
 const round = (value, precision) => {
     let multiplier = Math.pow(10, precision || 0);
     return Math.round(value * multiplier) / multiplier;
 }
+
+export {  toFahrenheit  };
+
